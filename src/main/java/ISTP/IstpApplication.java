a944b415294992bca8b08941d0fd2cb0024a0d08
chanhee01 <102280807+chanhee01@users.noreply.github.com>
2023-08-14 17:26:42 +0900

    [create] 프록젝트 생성#1

---
+++ src/main/java/ISTP/IstpApplication.java
@@ -0,0 +1,13 @@
+package ISTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstpApplication.class, args);
+	}
+
+}
