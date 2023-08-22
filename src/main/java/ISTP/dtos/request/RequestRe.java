dc1ad23683f28e4db37e2e3501855c8e31ffaef8
chanhee <102280807+chanhee01@users.noreply.github.com>
2023-08-22 14:54:20 +0900

    x

--- src/main/java/ISTP/dtos/request/RequestRe.java
+++ src/main/java/ISTP/dtos/request/RequestRe.java
@@ -1,5 +1,6 @@
 package ISTP.Dtos.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +10,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RequestRe {
 
+    @NotBlank
     private String sickness;
+    @NotBlank
     private String title;
+    @NotBlank
     private String content;
+    @NotBlank
     private String number;
+    @NotBlank
     private String hospital;
-    // private BloodType bloodType;
+    // private BloodType bloodType; // 여기서 라디오버튼 선택 어케하지??
+    @NotBlank
     private String relationship;
+    @NotBlank
     private String requests_blood_type;
 }
